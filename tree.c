466296e0ccbb4eb56a38c4ba8bf88a1146fb24a3
shanu97 <shanu97@users.noreply.github.com>
2016-06-30 22:02:02 +0530

    Incomplete
    Work in Progress

--- tree.c
+++ tree.c
@@ -1,15 +1,39 @@
 /* Tree implementation
  * C file: tree.c: Function definiton
  * Author: Shantanu Jha
- * Version: 1.1
+ * Version: 1.2
 */
 
 #include "tree.h"
+#include <assert.h>
 
 N_ent* root_global = NULL;
 
+/*T_ent* create_tree() {
+   T_ent* tree_list = (T_ent*) malloc (sizeof(T_ent));
+   if (tree_list == NULL)
+       return NULL;
+   tree_list->root = NULL;
+   return tree_list; 
+}
+*/
+
 int add_caller(int data) {
   return add_node(data, root_global);
+}
+
+void print_root() {
+  printf("%d\n", root_global->data);
+}
+
+N_ent* create_node(int data) {
+  N_ent* temp = (N_ent*) malloc (sizeof(N_ent));
+  if (temp == NULL)
+      return NULL;
+  temp->data = data;
+  temp->left = NULL;
+  temp->right = NULL;
+  return temp;
 }
 
 int add_node(int data, N_ent* node) {
@@ -18,12 +42,18 @@ int add_node(int data, N_ent* node) {
 	if (new_node == NULL)
 	    return 0;
 	new_node->data = data;
-	return addInternal(new_node, root_global);
+	return addInternal(new_node, node);
 }
 
 int addInternal(N_ent* new_node, N_ent* node) {
    if (node == NULL) {
+     //assert(node == root_global);
+     //root_global = (N_ent*) malloc (sizeof(N_ent));
      node = new_node;
+     root_global = node;
+     printf("%p, %p\n", (void*)node, (void*)root_global);
+     printf("%d\n", node->data);
+     printf("%d\n", root_global->data);
      return 1;
    }
    if(node->data == new_node->data) {
@@ -42,8 +72,11 @@ int addInternal(N_ent* new_node, N_ent* node) {
      return 0;
 }
 
+N_ent* search_caller(N_ent* to_search) {
+  return search(to_search, root_global);
+}
+
 N_ent* search(N_ent* to_search, N_ent* node) { 
-    node = root_global;
     if (node == NULL)
 	return NULL;
     if (to_search->data == node->data)
--- tree.h
+++ tree.h
@@ -1,7 +1,7 @@
 /* Tree implementation
  * Header file: tree.h: Structure declaration and required functions
  * Author: Shantanu Jha
- * Version: 1.1
+ * Version: 1.2
  */
 
 #include <stdio.h>
@@ -14,11 +14,17 @@ typedef struct _t_ent {
    struct _t_ent* right;
  } N_ent;
 
+N_ent* create_node(int data);
+
 int add_caller(int data);
+
+void print_root();
 
 int addInternal(N_ent* new_node, N_ent* node);
 
 int add_node(int data, N_ent* node);
+
+N_ent* search_caller(N_ent* to_search);
 
 N_ent* search(N_ent* to_search, N_ent* node);
 
