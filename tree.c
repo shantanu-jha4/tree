bd6ec44e552b362ed82a414e66a1414e414f75d6
shantanu-jha4 <shantanu-jha4@users.noreply.github.com>
2016-07-01 21:39:12 +0530

    UPDATED
    Tried a correction in addInternal method

--- tree.c
+++ tree.c
@@ -1,7 +1,7 @@
 /* Tree implementation
  * C file: tree.c: Function definiton
  * Author: Shantanu Jha
- * Version: 1.2
+ * Version: 1.3
 */
 
 #include "tree.h"
@@ -41,55 +41,38 @@ int add_node(int data, N_ent* node) {
 }
 
 int addInternal(N_ent* new_node, N_ent* node) {
-  
+    
     printf(" %p\n", (void*)node);
     if (node == NULL) {
-     //assert(node == root_global);
-     //root_global = (N_ent*) malloc (sizeof(N_ent));
-     //node = new_node;
-     //assert (node->right == NULL);
-     //assert (node->left == NULL);
      if (is_root_null == 0) {
      	printf("Root time: \n");
 	is_root_null = 1;
 	root_global = new_node;
 	root_global->left = NULL;
 	root_global->right = NULL;
+	node = root_global;
 	printf("Root Address: %p\n", (void*)root_global);
 	return 1;
      }
-     node = (N_ent*) malloc (sizeof(N_ent));
-     if (node == NULL)
-	 return 0;
-     node->data = new_node->data;
      node = new_node;
-    // printf("%p, %p\n", (void*)node, (void*)root_global);
-     //printf("Added: %d\n", node->data);
-     //printf("%d\n", root_global->data);
-     node->left = NULL;
-     node->right = NULL;
      assert (node->right == NULL);
      assert (node->left == NULL);
      return 1;
    }
-   //printf ("%d, %d\n" ,node->data, new_node->data);
    if(node->data == new_node->data) {
      free(new_node);
      return 2;
    }
 
    else if(node->data > new_node->data) {
-     //printf("Left..\n");
-     assert(node->left == NULL);
-     return addInternal(new_node, node->left);
+     assert(root_global->left == NULL);
+     node = node->left;
+     return addInternal(new_node, node);
    }
    else if(node->data < new_node->data) {
-     //printf("Right\n");
-     assert(node->right == NULL);
-
-     /*if (node->right)
-	 printf("Transfer: %d\n", node->right->data);*/
-     return addInternal(new_node, node->right);
+     assert(root_global->right == NULL);
+     node = node->right;
+     return addInternal(new_node, node);
    }
 
    else
